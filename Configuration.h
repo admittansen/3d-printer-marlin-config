32f9911063677b7751a40850a359d9f45a22e4d5
3d-skrivaren i labbet <3dskrivardatorn@admittansen.se>
2018-03-26 21:20:38 +0200

    Ändra Z_MAX_POS till 95. Standard enligt interwebz. Justerat bädden med skruvarna.

--- Configuration.h
+++ Configuration.h
@@ -335,7 +335,7 @@ const bool Z_MAX_ENDSTOP_INVERTING = true; // set to true to invert the logic of
 #define X_MIN_POS 0
 #define Y_MAX_POS 180
 #define Y_MIN_POS 0
-#define Z_MAX_POS 96.47
+#define Z_MAX_POS 95
 #define Z_MIN_POS 0
 
 #define X_MAX_LENGTH (X_MAX_POS - X_MIN_POS)
